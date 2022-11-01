6ab29ca35bfb2af808f03267a764885c123cc87c
hyuk0309 <bahngeh@naver.com>
2022-11-02 00:30:41 +0900

    feat : implement batch job

---
+++ src/main/kotlin/com/example/kotlinspringbatchpractice/job/CustomReaderJobConfig.kt
@@ -0,0 +1,62 @@
+package com.example.kotlinspringbatchpractice.job
+
+import com.example.kotlinspringbatchpractice.domain.Book
+import com.example.kotlinspringbatchpractice.domain.BookRepository
+import org.springframework.batch.core.Job
+import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
+import org.springframework.batch.core.configuration.annotation.StepScope
+import org.springframework.batch.item.ItemProcessor
+import org.springframework.batch.item.ItemWriter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class CustomReaderJobConfig(
+    val jobBuilderFactory: JobBuilderFactory,
+    val stepBuilderFactory: StepBuilderFactory,
+    val bookRepository: BookRepository
+) {
+    private final val CUSTOM_READER_JOB = "CUSTOM_READER_JOB"
+    private final val CUSTOM_READER_JOB_STEP = CUSTOM_READER_JOB + "_STEP"
+    private final val CHUNK_SIZE = 10
+
+    @Bean
+    fun customReaderJob(): Job {
+        return jobBuilderFactory.get(CUSTOM_READER_JOB)
+            .start(customReaderStep())
+            .build()
+    }
+
+    @Bean
+    fun customReaderStep(): Step {
+        return stepBuilderFactory.get(CUSTOM_READER_JOB_STEP)
+            .chunk<Book, Book>(CHUNK_SIZE)
+            .reader(reader())
+            .processor(processor())
+            .writer(writer())
+            .build()
+    }
+
+    @Bean
+    @StepScope
+    fun reader(): CustomItemReader {
+        return CustomItemReader()
+    }
+
+    @Bean
+    fun processor(): ItemProcessor<Book, Book> {
+        return ItemProcessor {
+            it.author = "Author. " + it.author
+            it
+        }
+    }
+
+    @Bean
+    fun writer(): ItemWriter<Book> {
+        return ItemWriter {
+            bookRepository.saveAll(it);
+        }
+    }
+}
