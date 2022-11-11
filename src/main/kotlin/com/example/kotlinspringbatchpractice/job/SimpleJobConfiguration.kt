a850973d815fd0fb646d6575a61ace79b8dc2a18
hyuk0309 <bahngeh@naver.com>
2022-11-11 23:40:18 +0900

    feat : study BATCH_JOB_INSTANCE table

--- src/main/kotlin/com/example/kotlinspringbatchpractice/job/SimpleJobConfiguration.kt
+++ src/main/kotlin/com/example/kotlinspringbatchpractice/job/SimpleJobConfiguration.kt
@@ -4,8 +4,10 @@ import org.apache.commons.logging.LogFactory
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
+import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory
 import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -19,15 +21,17 @@ class SimpleJobConfiguration(
     @Bean
     fun simpleJob(): Job {
         return jobBuilderFactory.get("simpleJob")
-            .start(simpleStep())
+            .start(simpleStep(null))
             .build()
     }
 
     @Bean
-    fun simpleStep(): Step {
+    @JobScope
+    fun simpleStep(@Value("#{jobParameters[requestDate]}")requestDate: String?): Step {
         return stepBuilderFactory.get("simpleStep")
             .tasklet { _, _ ->
                 log.info { ">>>>> This is Step1" }
+                log.info { ">>>>> requestData = {$requestDate}" }
                 RepeatStatus.FINISHED
             }
             .build()
