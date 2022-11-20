4b312d37693a14f21c79b8f5af2ac3e22fe14b0a
hyuk0309 <bahngeh@naver.com>
2022-11-20 18:05:31 +0900

    feat : implement spring batch integration test

---
+++ src/test/kotlin/com/example/kotlinspringbatchpractice/job/SimpleJobConfigurationTest.kt
@@ -0,0 +1,39 @@
+package com.example.kotlinspringbatchpractice.job
+
+import com.example.kotlinspringbatchpractice.TestBatchConfig
+import com.example.kotlinspringbatchpractice.job.step.SimpleJobTasklet
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.springframework.batch.core.JobParametersBuilder
+import org.springframework.batch.test.JobLauncherTestUtils
+import org.springframework.batch.test.context.SpringBatchTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import java.time.LocalDateTime
+import javax.batch.runtime.BatchStatus
+
+@SpringBatchTest
+@SpringBootTest(classes = [SimpleJobConfiguration::class, TestBatchConfig::class, SimpleJobTasklet::class])
+internal class SimpleJobConfigurationTest {
+
+    @Autowired
+    private lateinit var jobLauncherTestUtils: JobLauncherTestUtils
+
+    @Disabled("DB 용량 아끼기")
+    @Test
+    fun `simpleJob_통합_테스트`() {
+        //given
+        val now = LocalDateTime.now()
+
+        val jobParameters = JobParametersBuilder()
+            .addString("requestDate", now.toString())
+            .toJobParameters()
+
+        //when
+        val jobExecution = jobLauncherTestUtils.launchJob(jobParameters)
+
+        //then
+        assertThat(jobExecution.status.toString()).isEqualTo(BatchStatus.COMPLETED.toString())
+    }
+}
