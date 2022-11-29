360f0465ddd0fb22e70f5e6c49599fa49ba87c80
hyuk0309 <bahngeh@naver.com>
2022-11-30 00:39:20 +0900

    test : implement simplejob integration test

---
+++ src/test/kotlin/com/elvis/batch/job/simplejob/SimpleJobConfigurationTest.kt
@@ -0,0 +1,43 @@
+package com.elvis.batch.job.simplejob
+
+import com.elvis.batch.TestBatchConfig
+import com.elvis.batch.job.simplejob.step.SimpleJobTasklet
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import org.springframework.batch.core.JobParametersBuilder
+import org.springframework.batch.test.JobLauncherTestUtils
+import org.springframework.batch.test.context.SpringBatchTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import java.time.LocalDateTime
+import javax.batch.runtime.BatchStatus
+
+@SpringBatchTest
+@SpringBootTest(
+    classes = [
+        SimpleJobConfiguration::class,
+        SimpleJobTasklet::class,
+        TestBatchConfig::class]
+)
+internal class SimpleJobConfigurationTest {
+
+    @Autowired
+    private lateinit var jobLauncherTestUtils: JobLauncherTestUtils
+
+    @Test
+    fun `simpleJob_통합_테스트`() {
+        //given
+        val now = LocalDateTime.now()
+
+        val jobParameters = JobParametersBuilder()
+            .addString("requestDate", now.toString())
+            .toJobParameters()
+
+        //when
+        val jobExecution = jobLauncherTestUtils.launchJob(jobParameters)
+
+        //then
+        Assertions.assertThat(jobExecution.status.toString()).isEqualTo(BatchStatus.COMPLETED.toString())
+    }
+}
